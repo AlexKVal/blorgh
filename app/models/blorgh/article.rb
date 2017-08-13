7a02913a035a835a94579b40757356f575d97875
AlexKVal <alexkval@gmail.com>
2017-08-13 20:12:31 +0300

    add 'author_class' setting

--- app/models/blorgh/article.rb
+++ app/models/blorgh/article.rb
@@ -1,15 +1,17 @@
 module Blorgh
+  # Article model
   class Article < ApplicationRecord
     has_many :comments
 
     attr_accessor :author_name
-    belongs_to :author, class_name: 'User'
+    belongs_to :author, class_name: Blorgh.author_class.to_s
 
     before_validation :set_author
 
     private
-      def set_author
-        self.author = User.find_or_create_by(name: author_name)
-      end
+
+    def set_author
+      self.author = Blorgh.author_class.find_or_create_by(name: author_name)
+    end
   end
 end
--- lib/blorgh.rb
+++ lib/blorgh.rb
@@ -1,5 +1,10 @@
-require "blorgh/engine"
+require 'blorgh/engine'
 
+# Main module
 module Blorgh
-  # Your code goes here...
+  mattr_writer :author_class, instance_writer: false
+
+  def self.author_class
+    @@author_class.constantize
+  end
 end
